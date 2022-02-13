fe072c23aa698a662c862cb98bf467eb28ef9f00
Carbajal Marcos <manacarbajal@gmail.com>
2022-02-13 01:13:33 -0300

    path

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,36 @@
-import express, { Application } from 'express';
-import swaggerUI from 'swagger-ui-express';
-import swaggerJsDoc from 'swagger-jsdoc';
-import path from 'path';
-import dotenv from 'dotenv'
-dotenv.config()
+import express, { Application } from "express";
+import swaggerUI from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
+import path from "path";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const port = process.env.PORT || 3000;
-import { errorHandler } from './middlewares';
-import pokemon from './routes/pokemon';
+import { errorHandler } from "./middlewares";
+import pokemon from "./routes/pokemon";
 
 const swaggerSpec = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'Simple Pokedex Api',
-      version: '1.0.0',
+      title: "Simple Pokedex Api",
+      version: "1.0.0",
     },
     servers: [{ url: `http://localhost:${port}` }],
   },
-  apis: [`${path.join(__dirname, './routes/*.*')}`,],
+  apis: [`${path.join(__dirname, "./routes/*.*")}`],
 };
 // Boot express
 const app: Application = express();
-
+app.use(cors());
 app.use(pokemon);
 app.use(errorHandler);
 
 // Swagger
 
 app.use(
-  '/api-doc',
+  "/api-doc",
   swaggerUI.serve,
   swaggerUI.setup(swaggerJsDoc(swaggerSpec))
 );
